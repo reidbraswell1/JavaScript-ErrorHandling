094a3c2b09f4a19868639d6996519ebcedfa7f63
Ben <bbryant@truecoders.io>
2021-02-03 13:05:19 -0600

    Edited logs

--- app.js
+++ app.js
@@ -5,27 +5,29 @@ let repeatMessageThisManyTimes;
 class UserInputError extends Error {}
 
 function takeUserInput() {
-	userMessage = prompt("Please enter a message!");
-	//console.log(userMessage);
-	do {
-		try {
-			repeatMessageThisManyTimes = Number(prompt("How many times would you like your message to be repeated?"));
-			//console.log(repeatMessageThisManyTimes);
-			if (!repeatMessageThisManyTimes) {
-				throw new UserInputError("Not a Number.");
-			}
-		} catch (error) {
-			console.log(error.message);
-		}
-	} while (!repeatMessageThisManyTimes);
+  userMessage = prompt("Please enter a message!");
+  //console.log(userMessage);
+  do {
+    try {
+      repeatMessageThisManyTimes = Number(
+        prompt("How many times would you like your message to be repeated?")
+      );
+      console.log(repeatMessageThisManyTimes);
+      if (!repeatMessageThisManyTimes) {
+        throw new UserInputError("Not a Number.");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  } while (!repeatMessageThisManyTimes);
 }
 
 takeUserInput();
 
 function repeatMessage() {
-	for (let i = 0; i < repeatMessageThisManyTimes; i++) {
-		console.log(userMessage);
-	}
+  for (let i = 0; i < repeatMessageThisManyTimes; i++) {
+    console.log(userMessage + " " + i);
+  }
 }
 
-console.log(repeatMessage());
+repeatMessage();
