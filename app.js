fda7bdbdbc9b4f1992b2467abc2c9d760efa802c
reid <reidbras@gmail.com>
2021-09-03 20:11:42 -0500

    Updating code to add scroll pane log

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
 "use strict";
-let userMessage;
-let repeatMessageThisManyTimes;
 
 class UserInputError extends Error {}
 
 function takeUserInput() {
-  userMessage = prompt("Please enter a message!");
-  //console.log(userMessage);
+  let userMessage;
+  try {
+    userMessage = prompt("Please enter a message!");
+    if(userMessage == "") {
+      throw new UserInputError("Error - Message is blank.");
+    }
+  } catch (error) {
+    console.log(error.message);
+    document.getElementById("log").innerHTML=`${error.message}`;
+    document.getElementById("log").style.color="red";
+    return;
+  }
+  let repeatMessageText;
+  let repeatMessageNumber;
   do {
     try {
-      repeatMessageThisManyTimes = Number(
-        prompt("How many times would you like your message to be repeated?")
-      );
-      if (!repeatMessageThisManyTimes) {
-        throw new UserInputError("Not a Number.");
+      repeatMessageText = prompt("How many times would you like your message to be repeated?");
+      repeatMessageNumber = Number(repeatMessageText);
+      if (!repeatMessageNumber) {
+        throw new UserInputError(`Error: Not a Number - "${repeatMessageText}"`);
+      }
+      else {
+        repeatMessage(repeatMessageNumber, userMessage);
       }
     } catch (error) {
-      console.log(error.message);
+      console.log(`${error.message}`);
+      let log = document.getElementById("log");
+      log.innerText=`${error.message}`;
+      log.style.color="red";
+      return;
     }
-  } while (!repeatMessageThisManyTimes);
+  } while (!repeatMessageNumber);
 }
 
 takeUserInput();
 
-function repeatMessage() {
+function repeatMessage(repeatMessageThisManyTimes, userMessage) {
   for (let i = 0; i < repeatMessageThisManyTimes; i++) {
-    console.log(userMessage + " " + i);
+    console.log(`${i+1}. ${userMessage}`);
+    let textNode = document.createTextNode(`${i+1}. ${userMessage}`);
+    let br = document.createElement("br");
+    let log = document.getElementById("log");
+    log.appendChild(textNode);
+    log.appendChild(br);
+    log.style.color="black";
   }
 }
 
 repeatMessage();
+
+function sleep(miliseconds) {
+   var currentTime = new Date().getTime();
+
+   while (currentTime + miliseconds >= new Date().getTime()) {
+   }
+}
